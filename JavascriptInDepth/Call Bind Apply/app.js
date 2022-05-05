1a3ba38c09ae5b9eb02edaed14dc85bcce8bf378
Tamanpreet Singh <tamanpreetsingh13@gmail.com>
2022-05-05 19:48:15 +0530

    Learning apply and bind

--- JavascriptInDepth/Call Bind Apply/app.js
+++ JavascriptInDepth/Call Bind Apply/app.js
@@ -10,10 +10,10 @@ let userDetails = {
   //     console.log(this.name);
   //   },
 };
-let printDetails = function () {
-  console.log(this.name);
+let printDetails = function (state, country) {
+  console.log(this.name + " " + state + " " + country);
 };
-printDetails.call(userDetails);
+printDetails.call(userDetails, "Delhi", "India");
 
 let userDetails2 = {
   name: "Anjale Suneja",
@@ -22,5 +22,19 @@ let userDetails2 = {
 };
 //we apply call here to access object2 using object1
 //this is called function borrowing
+
 // userDetails.printDetails.call(userDetails2);
-printDetails.call(userDetails2);
+
+// printDetails.call(userDetails2, "Delhi", 'India');
+
+//The difference btw call and apply is in call we are passing a individual
+// elements like "delhi" in apply we can pass it in form of a array list.
+
+//eg of APPLY
+printDetails.apply(userDetails2, ["Delhi", "India"]);
+
+//BIND- we didnt call the func instead we stored its copy in newFun and then called newFun
+let newFun = printDetails.bind(userDetails, "Delhi", "India");
+newFun();
+
+//So with bind we can create a copy of our function and invoke it later.
