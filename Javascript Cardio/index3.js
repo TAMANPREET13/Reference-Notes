426622c1d7bad5280b4ebf445ac396c5de40f55b
Tamanpreet Singh <tamanpreetsingh13@gmail.com>
2022-05-07 14:09:30 +0530

    Learn to code in javascript 3

---
+++ Javascript Cardio/index3.js
@@ -0,0 +1,102 @@
+// CHALLENGE 1: ADD ALL NUMBERS
+// Return a sum of all parameters entered regardless of the amount of numbers - NO ARRAYS
+// ex. addAll(2,5,6,7) === 20
+
+//Solution- ES5 arguments & for loop
+function addAll() {
+  var args = Array.prototype.slice.call(arguments);
+  var total = 0;
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
+  return total;
+}
+
+//Solution-2- ...rest & forEach
+function addAlll(...numbers) {
+  let total = 0;
+  numbers.forEach((num) => {
+    total += num;
+  });
+  return total;
+}
+
+//Solution-3- ...rest & reduce
+function addAlll(...numbers) {
+  return numbers.reduce((acc, cur) => acc + cur);
+}
+// console.log(addAlll(2, 5, 6, 7));
+
+// CHALLENGE 2: SUM ALL PRIMES
+// Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
+// ex. sumAllPrimes(10) == 17
+
+function sumAllPrimes(n) {
+  let total = 0;
+
+  function checkForPrime(i) {
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  for (let i = 2; i <= n; i++) {
+    if (checkForPrime(i)) {
+      total += i;
+    }
+  }
+  return total;
+}
+
+// console.log(sumAllPrimes(10));
+
+// CHALLENGE 3: SEEK & DESTROY
+// Remove from the array whatever is in the following arguments. Return the leftover values in an array
+// ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
+
+//Solution -1 arguments,indexOf, filter
+function seekAndDestroy(arr) {
+  const args = Array.from(arguments);
+
+  function filterArr(arr) {
+    //Return true if NOT in array
+    return args.indexOf(arr) === -1;
+  }
+  return arr.filter(filterArr);
+}
+
+// console.log(seekAndDestroy([2, 3, 4, 6, 6, "hello"], 2, 6));
+
+//Solution-2 ...rest, filter & includes
+function seekAndDestroyyy(arr, ...rest) {
+  return arr.filter((val) => !rest.includes(val));
+}
+
+console.log(seekAndDestroyyy([2, 3, 4, 6, 6, "hello"], 2, 6));
+
+// CHALLENGE 4: SORT BY HEIGHT
+// Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
+// ex.
+// a = [-1, 150, 190, 170, -1, -1, 160, 180]
+// sortByHeight(a) == [-1, 150, 160, 170, -1, -1, 180, 190]
+
+function sortByHeight() {}
+
+// CHALLENGE 5: MISSING LETTERS
+// Find the missing letter in the passed letter range and return it. If all letters are present, return undefined
+// ex.
+// missingLetters("abce") == "d"
+// missingLetters("abcdefghjklmno") == "i"
+// missingLetters("abcdefghijklmnopqrstuvwxyz") == undefined
+
+function missingLetters() {}
+
+// CHALLENGE 6: EVEN & ODD SUMS
+// Take in an array and return an array of the sums of even and odd numbers
+// ex.
+// evenOddSums([50, 60, 60, 45, 71]) == [170, 116]
+
+function evenOddSums() {}
