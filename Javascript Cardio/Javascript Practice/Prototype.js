bf9c03a4816abddcf5602c5f8a5347f589ccd457
Tamanpreet Singh <tamanpreetsingh13@gmail.com>
2022-06-03 22:44:56 +0530

    Learnt about Prototype

---
+++ Javascript Cardio/Javascript Practice/Prototype.js
@@ -0,0 +1,11 @@
+let arr = ["Akshay", "Aditya"];
+
+let obj = {
+  name: "Taman",
+  city: "Delhi",
+  getIntro: function () {
+    console.log(this.name + "from " + this.city);
+  },
+};
+
+function fun() {}
